7544d830998bec7b9f4801a9d6013ec475b14ee3
Dharshanyaselva <dharshanya4@gmail.com>
2026-07-22 09:01:36 +0530

    Time: 64 ms (28.87%) | Memory: 22.3 MB (64.23%) - LeetSync

---
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack = []
+        prev_operator = "+"
+        curr_num = 0
+
+        for i in range(len(s)):
+
+            if s[i].isdigit():
+                curr_num = curr_num * 10 + int(s[i]) # what if we have double or triple digits
+            
+            if s[i] in "+-*/" or i == len(s) - 1:
+
+                if prev_operator == "+":
+                    stack.append(curr_num)
+                if prev_operator == "-":
+                    stack.append(-curr_num)
+                if prev_operator == "*":
+                    stack[-1] = stack[-1] * curr_num
+                if prev_operator == "/":
+                    stack[-1] = int(stack[-1] / curr_num) # don't use stack[-1]//curr_num it will give -5/2 to -3 but we want -2 so we use int, as it will remove anything after "." by giving -2
+
+                prev_operator = s[i]
+                curr_num = 0
+        
+        return sum(stack)
+        
