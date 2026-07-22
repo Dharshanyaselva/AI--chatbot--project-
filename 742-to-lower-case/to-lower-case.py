a7860c601f4a86cdb54c458b691c61142a7a3a33
Dharshanyaselva <dharshanya4@gmail.com>
2026-07-22 09:06:32 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (12.36%) - LeetSync

---
+++ 742-to-lower-case/to-lower-case.py
@@ -0,0 +1,6 @@
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+     return s.lower()   
+     print(Solution().toLowerCase("Hello"))   # Output: "hello"
+print(Solution().toLowerCase("here"))    # Output: "here"
+print(Solution().toLowerCase("LOVELY"))  # Output: "lovely"
